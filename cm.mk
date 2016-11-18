ebf7c4f3ed39b644516aa85eb948163eddb366fe
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 02:36:33 -0700

    m8: Remove enhanced NFC feature This is no longer used anywhere.

--- cm.mk
+++ cm.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, device/htc/b2wlj/full_b2wlj.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
