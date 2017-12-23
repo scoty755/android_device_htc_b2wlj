LineageOS/android_device_htc_m8

m8: Cleanup bluetooth config
* Remove unused flags
* MAX_L2CAP_CHANNELS defaults to 16

Change-Id: Ide4ecba5eda5fb96d2cf3bf74217334216287cb1

bluetooth/bdroid_buildcfg.h
@@ -17,16 +17,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QTI_SW TRUE
+#define BTM_DEF_LOCAL_NAME        "HTC One M8"
 
-#define BTM_DEF_LOCAL_NAME   "HTC One M8"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BLE_VND_INCLUDED          TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 #endif

scoty755/android_device_htc_b2wlj

bluetooth/bdroid_buildcfg.h
@@ -17,17 +17,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QTI_SW TRUE
+#define BTM_DEF_LOCAL_NAME        "HTC J butterfly"
 
-#define BTM_DEF_LOCAL_NAME   "HTC J butterfly"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BLE_VND_INCLUDED          TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 #endif
-