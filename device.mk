41dedbf3d7768d9edb5515e8b4cd3969c25157f7
scoty755 <scoty755@gmail.com>
2018-02-12 09:48:20 +0900

    b2wlj: Build HIDL HAL interfaces

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#               2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -39,6 +40,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8974 \
+    Snap
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
